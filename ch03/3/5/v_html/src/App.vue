52710fb60e9e0bd0b2bfd20396534ec1d40cb35a
koro74 <koro74@naver.com>
2024-12-30 13:08:22 +0900

    첫 커밋테스트

---
+++ ch03/3/5/v_html/src/App.vue
@@ -0,0 +1,13 @@
+<script>
+  export default{
+    data: function() {
+      return {
+        message: "<h1>Hello, Vue JS!</h1>",
+      };
+    },
+  }
+</script>
+
+<template>
+  {{  message }}
+</template>
