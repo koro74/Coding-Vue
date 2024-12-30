6a7ca7b148a1d16a6c4a8834833e37ffbdd75e48
koro74 <koro74@naver.com>
2024-12-30 15:14:47 +0900

    [vue.js] 3.3.5 디렉티브 사용하기
    [이슈] 코드 연습
    디렉티브 사용하기 : v-text

---
+++ ch03/3/5/v_text/src/App.vue
@@ -0,0 +1,21 @@
+<!--//
+3.3.5 디렉티브 사용하기
+- <script> 태그 영역에서 정의한 data 옵션 속성을 <template> 태그 영역에서
+  사용할 수 있게 하는 방법중에 디렉티브가 있습니다. (86 Page 디렉티브 사용하기 중)
+
+* v-text : v-text는 값을 단순하게 텍스트로 출력하게 하는 디렉티브입니다. (88 Page)
+//-->
+
+<script>
+  export default{
+    data: function() {
+      return {
+        message: "<h1>Hello, Vue JS!</h1>",
+      };
+    },
+  }
+</script>
+
+<template>
+  <div v-text="message"></div>
+</template>
