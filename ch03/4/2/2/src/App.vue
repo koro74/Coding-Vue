435419a01145f9275e86f362ace4e58cf285fffa
koro74 <koro74@naver.com>
2025-01-01 22:27:22 +0900

    [vue.js]  3.4.2 이벤트 다루기 [이슈] 코딩연습 이벤트 다루기 : v-on 이벤트 객체 값 전달

---
+++ ch03/4/2/2/src/App.vue
@@ -0,0 +1,20 @@
+<!--//
+3.4.2 v-on 디렉티브와 이벤트 객체 사용하기
+- 뷰에서는 이벤트 핸들러의 매개변수로 $event를 전달하면 이벤트 객체를 사용할 수 있습니다. (105 page)
+- 이벤트 객체와 일반 매개변수(값)를 전달할 경우
+//-->
+
+<script>
+  export default {
+    methods: {
+      clickHandler(event, message) {
+        console.log(event);
+        console.log(message);
+      },
+    },
+  }
+</script>
+
+<template>
+  <button type="button" @click="clickHandler($event, 'Hello')">클릭</button>
+</template>
