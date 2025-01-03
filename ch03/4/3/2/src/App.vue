8c5f50d07686ce19f9a7da293479cb32c0f48169
koro74 <koro74@naver.com>
2025-01-03 23:51:48 +0900

    [vue.js]  3.4.3 이벤트 다루기 [이슈] 코딩연습 이벤트 다루기 : v-on 수식어 사용하기

---
+++ ch03/4/3/2/src/App.vue
@@ -0,0 +1,21 @@
+<!--//
+3.4.3 v-on 디렉티브와 수식어 사용하기
+- 수식어(modifier)는 이벤트 처리 방식을 제어하는 데 사용하는 기능입니다. (106 page)
+ex) @이벤트_타입.수식어
+- 아래 코딩 예제는 @이벤트_타입.수식어 방식
+//-->
+
+<script>
+  export default {
+    methods: {
+      onKeyupHandler(e) {
+        console.log("Enter!");
+        console.log(e.key);
+      },
+    },
+  }
+</script>
+
+<template>
+  <input type="text" @keyup.enter="onKeyupHandler($event)" placeholder="@이벤트_타입.수식어" />
+</template>
