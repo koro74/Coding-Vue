879d6ea213cf46a1284e393445da568ee87e7dd3
koro74 <koro74@naver.com>
2025-01-05 00:46:54 +0900

    [vue.js]  3.5.3 폼 요소와 이벤트
    [이슈] 코딩연습
    폼 요소와 이벤트 : v-on keyup 이벤트

---
+++ ch03/5/3/keyup/src/App.vue
@@ -0,0 +1,20 @@
+<!--//
+3.5.3 폼 요소와 이벤트 (125 page)
+* v-on keyup 이벤트 (128 page)
+- 입력 요소에서 키보드의 키를 눌렀다가 뗐을 때 발생하는 이벤트입니다.
+- keyup 이벤트는 주로 사용자의 키 입력에 따라 코드를 동적으로 처리해야 하는 경우에 유용합니다.
+//-->
+
+<script>
+  export default {
+    methods: {
+      onKeyupHandler: function() {
+        console.log("Keyup Event!");
+      },
+    },
+  }
+</script>
+
+<template>
+  <input type="text" v-on:keyup="onKeyupHandler" />
+</template>
