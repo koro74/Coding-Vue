bce54347eb36345837f74fa2539d4c6e675dc776
koro74 <koro74@naver.com>
2025-01-04 18:13:09 +0900

    [vue.js]  3.5.2 폼 요소 다루기
    [이슈] 코딩연습
    폼 요소 다루기 : v-model input, textarea

---
+++ ch03/5/2/input/src/App.vue
@@ -0,0 +1,38 @@
+<!--//
+3.5.2 v-model 디렉티브로 폼 요소 다루기
+- 뷰에서는 입력한 요소의 값을 가져올 때 V-MODEL 디렉티브를 사용합니다.
+- v-model 디렉티브는 <script> 태그 영역에 정의한 데이터 속성과 <template> 태그 영역에
+작성한 HTML 요소가 양방향으로 서로 연결되어 데이터 속성의 값을 자동으로 업데이트 합니다.(양방향 데이터 바인딩 115 page) 
+
+* 한 줄 입력 요소의 값 가져오기 (116 page)
+- <input type="text">
+//-->
+
+<script>
+  export default {
+    data: function() {
+      return {
+        uid: "",
+        upw: "",
+      };      
+    },
+
+    methods: {
+      login: function() {
+        console.log(`\`\` 기호 사용 시 id: ${this.uid}`);
+        console.log("큰따옴표 사용 시 id : ${this.uid}");
+        console.log('작은따옴표 사용 시 id : ${this.uid}');
+        console.log("${} 사용 안하고 this.uid 로 사용 하기 ID >>>> " + this.uid);
+        console.log(`pw: ${this.upw}`);
+      },
+    },
+  }
+</script>
+
+<template>
+  <form id="loginForm">
+    <label for="uid">아이디 : <input type="text" id="uid" v-model="uid" /></label>
+    <label for="upw">비밀번호 : <input type="password" id="upw" v-model="upw" /></label>
+    <button type="button" v-on:click="login">로그인</button>
+  </form>
+</template>
---
+++ ch03/5/2/textarea/src/App.vue
@@ -0,0 +1,35 @@
+<!--//
+3.5.2 v-model 디렉티브로 폼 요소 다루기
+- 뷰에서는 입력한 요소의 값을 가져올 때 V-MODEL 디렉티브를 사용합니다.
+- v-model 디렉티브는 <script> 태그 영역에 정의한 데이터 속성과 <template> 태그 영역에
+작성한 HTML 요소가 양방향으로 서로 연결되어 데이터 속성의 값을 자동으로 업데이트 합니다.(양방향 데이터 바인딩 115 page) 
+
+* 여러 줄 입력 요소의 값 가져오기 (117 page)
+- v-model 디렉티브를 사용하면 초기값을 초기 렌더링 화면에서 보여주거나(<script> -> <template>)
+사용자가 입력한 값을 업데이트(<template> -> <script>)할 수 있습니다.
+- <textarea>
+//-->
+
+<script>
+  export default {
+    data: function() {
+      return {
+        message: "",
+      };
+    },
+
+    methods: {
+      printData: function() {
+        console.log("ID : " + this.message);
+        console.log(`ID : ${this.message}`);
+      },
+    },
+  }
+</script>
+
+<template>
+  <form id="loginForm">
+    <textarea v-model="message"></textarea>
+    <button type="button" v-on:click="printData">데이터 출력</button>
+  </form>
+</template>
