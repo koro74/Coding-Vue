c2fd225a097bc40b844703f39db6271cc24bd992
koro74 <koro74@naver.com>
2025-01-05 03:27:41 +0900

    [vue.js]  3.6.2 감시자 속성
    [이슈] 코딩연습
    감시자 속성 : watch - 기본 사용법

---
+++ ch03/6/2/basic-watch/src/App.vue
@@ -0,0 +1,28 @@
+<!--//
+3.6.2 감시자 속성 (133 page)
+- 감시자 속성(watch)도 옵션스 API에서 제공하는 속성 중 하나로, watch 옵션 속성으로 정의해 사용할 수 있습니다.
+- watch 옵션 속성은 데이터의 변경을 감시하고, 변경이 감지될 때마다 특정 동작을 수행할 수 있게 합니다.
+
+* 기본 사용법
+- watch 옵션 속성은 데이터 속성과 같은 식별자를 사용하는 함수를 값으로 가집니다. (134 page)
+//-->
+
+<script>
+  export default {
+    data: function() {
+      return {
+        inputStr: "", // inputStr 데이터 속성을 감시
+      };
+    },
+
+    watch: {
+      inputStr(newValue, oldValue) { // watch 옵션 속성에 inputStr 함수를 정의해야한다.
+        console.log(`old value : ${oldValue}`);
+        console.log(`newvalue : ${newValue}`);
+      },
+    },
+  }
+</script>
+<template>
+  <input type="text" v-model="inputStr" />
+</template>
