e1f74a219d944a4b5f567bb520b4032c68187b78
koro74 <koro74@naver.com>
2025-01-05 01:58:26 +0900

    [vue.js]  3.6.1 계산된 속성
    [이슈] 코딩연습
    계산된 속성 :  computed

---
+++ ch03/6/1/computed/src/App.vue
@@ -0,0 +1,32 @@
+<!--//
+3.6.1 계산된 속성 (130 page)
+- 계산된 속성(computed)은 옵션스 API에서 제공하는 속성 중 하나로, computed 옵션 속성으로 정의해 사용할 수 있습니다.
+- computed 옵션 속성은 컴포넌트에서 자주 사용하는 데이터를 캐시해 애플리케이션의 성능을 향상시키는데 목적이 있습니다.
+- computed 옵션 속성은 함수를 값으로 가지는 methods 옵션 속성과 같은 속성을 값으로 가집니다.
+- computed 옵션 속성에 정의 함수는 내부에서 항상 데이터를 참조하는데,  한 번 참조한 데이터는 값이 변경되기 전까지
+computed 옵션 속성에 의해 캐싱됩니다.
+- 아래 코드는 computed 옵션 속성을 적용 코드입니다.
+//-->
+
+<script>
+  export default {
+    data: function() {
+      return {
+        firstName: "Gildong",
+        lastName: "Hong",
+      };
+    },
+
+    computed:{
+      fullName: function() {
+        console.log("computed fullName");
+        return `${this.lastName} ${this.firstName}`;
+      },
+    },
+  }
+</script>
+
+<template>
+  <h1>{{ fullName }}</h1>
+  <h1>{{ fullName }}</h1>
+</template>
