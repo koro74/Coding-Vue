6705ec4287c6d80ef5cfdc310cb6a30630502c65
koro74 <koro74@naver.com>
2025-01-05 01:38:17 +0900

    [vue.js]  3.6.1 계산된 속성
    [이슈] 코딩연습
    계산된 속성 :  no computed

--- ch03/6/1/no-computed/src/App.vue
+++ ch03/6/1/no-computed/src/App.vue
@@ -1,47 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<!--//
+3.6.1 계산된 속성 (130 page)
+- 계산된 속성(computed)은 옵션스 API에서 제공하는 속성 중 하나로, computed 옵션 속성으로 정의해 사용할 수 있습니다.
+- computed 옵션 속성은 컴포넌트에서 자주 사용하는 데이터를 캐시해 애플리케이션의 성능을 향상시키는데 목적이 있습니다.
+- 아래 코드는 computed 옵션 속성을 적용하기 전 코드입니다.
+//-->
+
+<script>
+  export default {
+    data: function() {
+      return {
+        firstName: "Gildong",
+        lastName: "Hong",
+      };
+    },
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <!--// 뷰 애플리케이션은 <template> 태그에 데이터를 바인딩할 때마다 각 데이터 속성의 값을 참조합니다. //-->
+  <h1>{{ lastName }} {{ firstName }}</h1>
+  <h1>{{ lastName }} {{ firstName }}</h1>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
