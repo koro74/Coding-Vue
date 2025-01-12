eda84b91cffaf062754133ddbe7f1104c66594e3
koro74 <koro74@naver.com>
2025-01-12 16:13:19 +0900

    [vue.js]  5.3 컴포넌트 생명주기 이해하기
    [이슈] 코딩연습
    1) 5.3.1 라이프사이클 훅

---
+++ ch05/03/1/lifecycle/src/App.vue
@@ -0,0 +1,44 @@
+<!--//
+5.3.1 라이프사이클 훅 (196 page)
+- 라이프사이클 훅 호출 확인
+//-->
+
+<script>
+  export default {
+    beforeCreate: function() {
+      console.log("beforeCreate");  
+    },
+
+    created: function() {
+      console.log("created");
+    },
+
+    beforeMount: function() {
+      console.log("beforeMount");
+    },
+
+    mounted: function() {
+      console.log("mounted");
+    },
+
+    beforeUpdate: function() {
+      console.log("beforeUpdate");
+    },
+
+    updated: function() {
+      console.log("updted");
+    },
+
+    beforeUnmount: function() {
+      console.log("beforeUnmount");
+    },
+
+    unmounted: function() {
+      console.log("unmounted");
+    },
+  }
+</script>
+
+<template>
+
+</template>
