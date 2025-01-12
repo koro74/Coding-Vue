8f30aa1c33c7e139e3d31c1b8ca0c32663d110b3
koro74 <koro74@naver.com>
2025-01-12 22:47:49 +0900

    [vue.js]  5.3 컴포넌트 생명주기 이해하기
    [이슈] 코딩연습
    1) 5.3.1 라이프사이클 훅

---
+++ ch05/03/1/locallifecycle/src/App.vue
@@ -0,0 +1,68 @@
+<!--//
+5.3.1 라이프사이클 훅 (196 page)
+- 라이프사이클 훅 호출 확인 (198 page)
+- 지역 등록을 이용 
+//-->
+
+<script>
+  import FirstChild from '@/components/FirstChild.vue'; // 1)
+  // 1) FirstChild  컴포넌트를 지역 등록하기 위해 불러옵니다.
+
+  export default {
+    components: {
+      FirstChild, // 2)
+      // 2) 불러온 FirstChild 컴포넌트를 components 옵션 속성으로 지역 등록합니다
+    },
+
+    data: function() {
+      return {
+        visible: true,
+      };
+    },
+
+    beforeCreate: function() {
+      console.log("beforeCreate");  
+    },
+
+    created: function() {
+      console.log("created");
+      // 3) 컴포넌트가 생성되고 3초 뒤에 visible 데이터의 값을 false로     
+      setTimeout(() => {     
+        this.visible = false;        
+        console.log("3초 후 ===============> " + this.visible);
+      }, 3000);
+    },
+
+    beforeMount: function() {
+      console.log("beforeMount");
+    },
+
+    mounted: function() {
+      console.log("mounted");
+    },
+
+    beforeUpdate: function() {
+      console.log("beforeUpdate");
+    },
+
+    updated: function() {
+      console.log("updted");
+    },
+
+    beforeUnmount: function() {
+      console.log("beforeUnmount");
+    },
+
+    unmounted: function() {
+      console.log("unmounted");
+    },
+  }
+</script>
+
+<template>
+  <FirstChild v-if="visible" /> 
+  <!--//
+    visible 데이터의 값이 true일 때만 FirstChild 컴포넌트를 렌더링합니다.
+    3)에 의해 3초 뒤 FirstChild 컴포넌트가 DOM에서 제거됩니다.
+  //-->
+</template>
---
+++ ch05/03/1/locallifecycle/src/components/FirstChild.vue
@@ -0,0 +1,26 @@
+<!--//
+    1. FirstChild.vue 파일 생성
+    - beforeUnmount(),  unmounted() 코드 추가
+//-->
+<script>
+    export default {
+        beforeUnmount: function() {
+            console.log("beforeUnmount");
+        },
+
+        unmounted: function() {
+            console.log("unmounted");
+        },
+    }
+</script>
+
+<template>
+    <h1>First Child</h1>
+</template>
+
+<style>
+    h1 {
+        color: green;
+        font-weight: bold;
+    }
+</style>
