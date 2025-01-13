c58d68056ead19fa3197df800eae97043dcbcf44
koro74 <koro74@naver.com>
2025-01-13 17:44:17 +0900

    [vue.js]  5.3 컴포넌트 생명주기 이해하기
    [이슈] 코딩연습
    3) 5.3.2 탬플릿 ref - 컴포넌트 참조

---
+++ ch05/03/2/3/src/App.vue
@@ -0,0 +1,19 @@
+<!--//
+  5.3 컴포넌트 생명주기 이해하기
+  5.3.2 템플릿 ref
+  - 컴포넌트 참조 (202 page)
+//-->
+
+<script>
+import FirstChild from '@/components/FirstChild.vue';
+
+export default {
+  components: {
+    FirstChild,
+  },
+}
+</script>
+
+<template>
+  <FirstChild ref="child" />
+</template>
---
+++ ch05/03/2/3/src/components/FirstChild.vue
@@ -0,0 +1,32 @@
+<script>
+    export default {
+        data: function() {
+            return {
+                num: 5,
+            };
+        },
+
+        computed: {
+            doubleNum: function() {
+                return this.num * 2;
+            },
+        },
+
+        methods: {
+            increment: function() {
+                this.num++;
+            },
+        },
+    }
+</script>
+
+<template>
+    <h1><span>components ref : </span>{{ num }}</h1>
+</template>
+
+<style>
+    h1 span {
+        color: blueviolet;
+        font-weight: bold;
+    }
+</style>
