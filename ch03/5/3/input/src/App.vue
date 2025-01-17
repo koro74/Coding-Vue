66f703d2fac2e5e100d9e80502136eec2e7cc536
koro74 <koro74@naver.com>
2025-01-18 01:10:36 +0900

    [vue.js] 3.5.3 폼 요소와 이벤트
    [이슈] 코딩연습
    폼 요소와 이벤트 : v-on input 이벤트

---
+++ ch03/5/3/input/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+  export default {
+    data: function() {
+      return {
+        message: "",
+      };
+    },
+
+    methods: {
+      onChangeHandler($event) {
+        this.message = $event.target.value;
+      },
+    },
+  }
+</script>
+
+<template>
+  <input type="text" v-on:input="onChangeHandler($event)" />
+  <br>
+  {{ message }}
+</template>
