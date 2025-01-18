ce18e662e4c3581f9b1833647a49748f219150d2
koro74 <koro74@naver.com>
2025-01-19 04:09:22 +0900

    [vue.js]  5.4 컴포넌트에 속성과 이벤트 정의하기
    [이슈] 코딩연습
    1) 5.4.2 컴포넌트의 사용자 정의 이벤트
    이벤트 발신 인자와 매개변수

---
+++ ch05/04/parameter/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  import UserProfile from '@/components/UserProfile.vue';
+
+  export default {
+    components: {
+      UserProfile,
+    },
+
+    methods: {
+      printHello: function(name, age) {
+      alert(`안녕하세요! 저는 ${name}이고, 나이는 ${age}살입니다.`);
+      },
+    },
+  }
+</script>
+
+<template>
+  <!--// print-hello 이벤트만 정의되어 있음 //-->
+  <UserProfile v-on:print-hello="(name, age) => printHello(name, age)" />
+</template>
---
+++ ch05/04/parameter/src/components/UserProfile.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {
+        emits: ["print-hello"],
+        methods: {
+            parentEventCall: function() {
+                // emits 옵션 속성으로 print-hello 이벤트만 명시
+                // 명시되지 않은 onPrint-hello 이벤트 호출 시 경고 발생
+                this.$emit("print-hello", "철수", 30);
+            },
+        },
+    }
+</script>
+<template>
+    <button v-on:click="parentEventCall">클릭</button>
+</template>
