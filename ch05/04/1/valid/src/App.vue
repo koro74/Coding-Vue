b03182af733fef4d52c10877823760201b200403
koro74 <koro74@naver.com>
2025-01-19 00:01:58 +0900

    [vue.js]  5.4 컴포넌트에 속성과 이벤트 정의하기
    [이슈] 코딩연습
    1) 5.4.1 컴포넌트의 사용저 정의 속성
    props 사용자 정의 속성의 유효성 검사

---
+++ ch05/04/1/valid/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  import UserProfile from '@/components/UserProfile.vue';
+
+  export default {
+    components: {
+      UserProfile,
+    },
+
+    data: function() {
+      return {
+        userName: "김철수",
+        age: 30,
+      };
+    },
+  }
+</script>
+
+<template>
+  <UserProfile v-bind:user-name="userName" />
+</template>
---
+++ ch05/04/1/valid/src/components/UserProfile.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+      props: {
+        name: String,   // name 속성의 값이 문자열인지 검사
+        age: [String, Number], // age 속성의 값이 문자열이나 숫자인지 검사
+      },
+    }
+</script>
+
+<template>
+    <p>이름 : {{ name }}</p>
+    <p>나이 : {{ age }}</p>
+</template>
