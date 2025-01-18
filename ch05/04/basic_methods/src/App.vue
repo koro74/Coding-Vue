f1d7d3ddb3249cc57868c9ff6f365bd347b80627
koro74 <koro74@naver.com>
2025-01-19 02:51:18 +0900

    [vue.js]  5.4 컴포넌트에 속성과 이벤트 정의하기
    [이슈] 코딩연습
    1) 5.4.2 컴포넌트의 사용자 정의 이벤트
    이벤트 발신

---
+++ ch05/04/basic_methods/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+  import UserProfile from '@/components/UserProfile.vue';
+
+  export default {
+    components: {
+      UserProfile,
+    },
+
+    methods: {
+      printHello: function() {
+        alert("안녕하세요!");
+      },
+    },
+  }
+</script>
+
+<template>
+  <UserProfile v-on:print-hello="printHello" />
+</template>
---
+++ ch05/04/basic_methods/src/components/UserProfile.vue
@@ -0,0 +1,12 @@
+<script>
+    export default {
+        methods: {
+            parentEventCall: function() {
+                this.$emit("print-hello");
+            },
+        },
+    }
+</script>
+<template>
+    <button v-on:click="parentEventCall">클릭</button>
+</template>
