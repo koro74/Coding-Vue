5ec97edd38a84cdf2419b965035fc22c82e745b3
koro74 <koro74@naver.com>
2025-01-19 15:23:43 +0900

    [vue.js]  5.5 컴포넌트의 데이터 효율적으로 다루기
    [이슈] 코딩연습
    5.5.1 컴포넌트의 데이터 공유하기 - provide

---
+++ ch05/05/1/basic_1/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+  import FirstChild from '@/components/FirstChild.vue';
+
+  export default {
+    components: {
+      FirstChild,
+    },
+
+    provide: function() {
+      return {
+        message: this.message,
+        reversedMessage: this.reversedMessage,
+      };
+    },
+
+    data: function() {
+      return {
+        message: "Hello, Vue JS!",
+        reversedMessage: "Vue JS provide TEST!",
+      };
+    },
+  }
+</script>
+
+<template>
+  <FirstChild />
+</template>
