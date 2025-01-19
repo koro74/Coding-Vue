55bd6c6d5087d28cafa0555743be3133f306865b
koro74 <koro74@naver.com>
2025-01-19 16:33:23 +0900

    [vue.js]  5.5 컴포넌트의 데이터 효율적으로 다루기
    [이슈] 코딩연습
    5.5.1 컴포넌트의 데이터 공유하기 - provide

---
+++ ch05/05/1/basic_2/src/components/FirstChild.vue
@@ -0,0 +1,10 @@
+<script>
+    export default {
+        inject: ["message", "reversedMessage"],
+    }
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+    <h2>{{ reversedMessage }}</h2>
+</template>
