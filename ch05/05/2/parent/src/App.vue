12e541353e0a6e59dbd9a3ac325b87e205909b66
koro74 <koro74@naver.com>
2025-01-25 02:02:32 +0900

    [vue.js]  5.5 컴포넌트의 데이터 효율적으로 다루기
    [이슈] 코딩연습
    1) 5.5.2 컴포넌트의 데이터 참조하기 - 부모 컴포넌트에서 자식 컴포넌트 접근하기

---
+++ ch05/05/2/parent/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+  import FirstChild from '@/components/FirstChild.vue';
+
+  export default {
+    components: {
+      FirstChild,
+    },
+
+    mounted: function() { /* 2 */
+      console.log(this.$refs.childRef.childNum); // 0
+      console.log(this.$refs.childRef.childNumOddAdd); // 짝수
+      console.log(this.$refs.childRef.getChildNum()); // 0
+    },
+  }
+</script>
+
+<template>
+  <FirstChild ref="childRef" /> <!--// 1 //-->
+</template>
---
+++ ch05/05/2/parent/src/components/FirstChild.vue
@@ -0,0 +1,25 @@
+<script>
+    export default {
+        data: function() {
+            return {
+                childNum: 0,
+            };
+        },
+
+        computed: {
+            childNumOddAdd: function() {
+                return this.childNum % 2 === 0 ? "짝수" : "홀수";
+            },
+        },
+
+        methods: {
+            getChildNum: function() {
+                return this.childNum;
+            },
+        },
+    }
+</script>
+
+<template>
+    <h1>FirstChild</h1>
+</template>
