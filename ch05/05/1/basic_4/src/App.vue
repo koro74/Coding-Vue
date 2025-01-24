e56578e573e933af3f0d627f671ab0909739d253
koro74 <koro74@naver.com>
2025-01-25 01:11:00 +0900

    [vue.js]  5.5 컴포넌트의 데이터 효율적으로 다루기
    [이슈] 코딩연습
    1) 5.5.1 컴포넌트의 데이터 공유하기 - provide에 Symbol 사용하기

---
+++ ch05/05/1/basic_4/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+  import FirstChild from '@/components/FirstChild.vue';
+  import { message, reversedMessage } from "@/keys.js";
+
+  export default {
+    components: {
+      FirstChild,
+    },
+
+    provide: function() {
+      return {
+        [message]: this.message,
+        [reversedMessage]: this.reversedMessage,
+      };
+    },
+
+    data: function() {
+      return {
+        message: "Hello, Vue JS!",
+      };
+    },
+
+    computed: {
+      reversedMessage: function() {
+        return this.message.split("").reverse().join("");
+      },
+    },
+  }
+</script>
+
+<template>
+  <FirstChild />
+</template>
---
+++ ch05/05/1/basic_4/src/components/FirstChild.vue
@@ -0,0 +1,22 @@
+<script>
+    import { message, reversedMessage } from "@/keys.js";
+
+    export default {
+        inject: {
+            customMessage: {
+                from: message,  // import로 불러온 symbol 값
+                default: "default Symbol value",
+            },
+
+            customReversedMessage: {
+                from: reversedMessage, // import로 불러온 symbol 값
+                default: "default Symbol value",
+            },
+        },
+    }
+</script>
+
+<template>
+    <h1>{{ customMessage }}</h1>
+    <h2>{{ customReversedMessage }}</h2>
+</template>
