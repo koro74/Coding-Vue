adce24300932fb66d1ad9ff1de423ab0e30acf04
koro74 <koro74@naver.com>
2025-02-08 23:53:22 +0900

    [vue.js]  5.6.2 이름이 있는 글롯
    [이슈] 코딩연습
    5.6.2 이름이 있는 슬롯

---
+++ ch05/06/2/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+  import DefaultLayout from "@/components/DefaultLayout.vue";
+
+  export default {
+    components: {
+      DefaultLayout,
+    },
+  }
+</script>
+
+<template>
+  <DefaultLayout>
+    <!--// Header //-->
+    <template v-slot:header>
+      <header>
+        <h1>Header</h1>
+      </header>
+    </template>
+
+    <!--// Main //-->
+    <template v-slot:main>
+      <main>
+        <h1>Main</h1>
+      </main>
+    </template>
+
+    <!--// footer //-->
+    <template v-slot:footer>
+      <footer>
+        <h1>footer</h1>
+      </footer>
+    </template>
+  </DefaultLayout>
+</template>
